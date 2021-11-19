8c8647d05839b0335264da995f8357bc857b5043
Maria Julia Nolasco <mariajdmn@gmail.com>
2021-11-18 23:02:34 -0300

    Dropdown Feito e Pagina da Empresa Linkada

---
+++ WebApplication1/Controllers/EmpresaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        private readonly ILogger<EmpresaController> _logger;
+
+        public EmpresaController(ILogger<EmpresaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/EmpresaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class EmpresaModel
+    {
+        public string? Nome { get; set; }
+
+        public string? NomeFantasia { get; set; }
+
+        public int? Codigo { get; set; }
+
+        public string? CNPJ { get; set; }
+    }
+}
