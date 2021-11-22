f794a4a099a24bd0027d4c7960370f1f4be17126
Maria Julia Nolasco <mariajdmn@gmail.com>
2021-11-22 15:21:29 -0500

    Melhorias no form de empresa e melhorias do alerta do botão redireciona

--- WebApplication1/Models/EmpresaModel.cs
+++ WebApplication1/Models/EmpresaModel.cs
@@ -6,8 +6,17 @@
 
         public string? NomeFantasia { get; set; }
 
-        public int? Codigo { get; set; }
+        public int Codigo { get; set; }
 
         public string? CNPJ { get; set; }
+
+        public EmpresaModel(string Nome, string NomeFantasia, int Codigo, string CNPJ)
+        {
+            this.Nome = Nome;
+            this.NomeFantasia = NomeFantasia;
+            this.Codigo = Codigo;
+            this.CNPJ = CNPJ;
+        }
+        
     }
 }
