340d4987d1e69b4ebb213d08eca1bdc18eb22df0
Maria Julia Nolasco <mariajdmn@gmail.com>
2021-11-23 17:04:16 -0500

    PessoaModel criado

---
+++ WebApplication1/Models/PessoaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Models
+{
+    public class PessoaModel
+    {
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int Codigo { get; set; }
+        public int QuantidadeFilhos{ get; set; }
+        public DateTime DataNascimento { get; set;}
+        public decimal? Salario { get; set;}
+    }
+}
