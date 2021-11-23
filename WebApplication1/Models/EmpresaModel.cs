904cc2b6a6595257bf0f6f347b2723494e534477
Maria Julia Nolasco <mariajdmn@gmail.com>
2021-11-23 16:25:23 -0500

    Melhoria no funcionamento da checkbox do form de empresa

--- WebApplication1/Models/EmpresaModel.cs
+++ WebApplication1/Models/EmpresaModel.cs
@@ -3,10 +3,10 @@
     public class EmpresaModel
     {
 
-        public string Nome { get; set; }
-        public string NomeFantasia { get; set; }
+        public string? Nome { get; set; }
+        public string? NomeFantasia { get; set; }
         public int Codigo { get; set; }
-        public string CNPJ { get; set; }
+        public string? CNPJ { get; set; }
 
         /* Não sei pq se tiver constrtor dá merda
          * public EmpresaModel(string Nome, string NomeFantasia, int Codigo, string CNPJ)
